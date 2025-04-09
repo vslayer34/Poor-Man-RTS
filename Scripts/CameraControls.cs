eb13cd576a29d866056134a30b4fe19e053eaad0
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2025-04-09 18:31:50 +0200

    Camera Controler

--- Scripts/CameraControls.cs
+++ Scripts/CameraControls.cs
@@ -6,40 +6,70 @@ public partial class CameraControls : Camera2D
     [Export]
     private float _cameraSpeed = 10.0f;
 
-    private Vector2 _x;
+    private Vector2 _screenMoveDirection;
+
+    private float _screenFingerSpeed;
 
 
 
-    // Game Loop MEthods---------------------------------------------------------------------------
+    // Game Loop Methods---------------------------------------------------------------------------
     public override void _UnhandledInput(InputEvent @event)
     {
         if (@event is InputEventScreenDrag drag)
         {
-            _x = drag.Relative;
+            // GD.Print(drag.Relative);
+            // GD.Print(drag.IsCanceled());
+            // GD.Print(drag.ScreenVelocity);
+            Vector2 deadZone = new Vector2(10.0f, 10.0f);
+
+            // if (drag.ScreenRelative.LengthSquared() > deadZone.LengthSquared())
+            // {
+                _screenMoveDirection = drag.ScreenRelative;
+            // }
+
+            // GD.Print(drag.ScreenVelocity);
+            GD.Print(drag.Velocity.Length());
+            _screenFingerSpeed = Mathf.Clamp(drag.Velocity.Length(), _cameraSpeed / 4.0f, _cameraSpeed);
+
+            if (drag.ScreenRelative == Vector2.Zero)
+            {
+                _screenMoveDirection = Vector2.Zero;
+            }
         }
 
 
-        if (@event is InputEventScreenTouch tocuh)
+        if (@event is InputEventScreenTouch touch)
         {
-            // GD.Print("Touch");
-            if (!tocuh.Canceled)
+            // GD.Print(touch.Position);
+            // Reset the camera movement
+            // GD.Print(touch.Index);
+            GD.Print(touch.IsCanceled());
+
+            if (touch.IsCanceled())
             {
-                GD.Print("CAncelled");
-                _x = Vector2.Zero;
+                
             }
+            _screenMoveDirection = Vector2.Zero;
+            // if (!tocuh.Canceled)
+            // {
+            //     GD.Print("CAncelled");
+            //     _x = Vector2.Zero;
+            // }
         }
     }
 
     public override void _Process(double delta)
     {
-        MoveCamera(_x, (float)delta);
+        MoveCamera(_screenMoveDirection, (float)delta);
     }
 
     // Member Methods------------------------------------------------------------------------------
 
     private void MoveCamera(Vector2 direction, float delta)
     {
-        Position += direction.Normalized() * _cameraSpeed * delta;
+        direction = new Vector2(-direction.X, -direction.Y);
+        // Position += direction.Normalized() * _cameraSpeed * delta * _screenFingerSpeed;
+        Position += direction.Normalized() *  delta * _screenFingerSpeed;
         // GD.Print(direction);
     }
 }
