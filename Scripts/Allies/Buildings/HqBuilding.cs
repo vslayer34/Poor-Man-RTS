f2e20b26d29019a839518b4dc64aef12355c06f8
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2025-04-14 18:38:53 +0200

    Added spawn Point to the HQ buildings

--- Scripts/Allies/Buildings/HqBuilding.cs
+++ Scripts/Allies/Buildings/HqBuilding.cs
@@ -6,6 +6,18 @@ using System;
 namespace PoorManRTS.Allies.Buildings;
 public partial class HqBuilding : Node2D, IBuildUnits
 {
+    private Marker2D _spawnPoint;
+
+
+
+    // Game Loop Methods---------------------------------------------------------------------------
+
+    public override void _Ready()
+    {
+        base._Ready();
+        _spawnPoint = GetNode<Marker2D>("SpawnPoint");
+    }
+
 
     // Interface Methods---------------------------------------------------------------------------
 
