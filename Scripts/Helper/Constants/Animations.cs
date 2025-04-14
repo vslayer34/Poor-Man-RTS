215d1dc41bebac62817c23c395143fc1dbd2985c
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2025-04-14 18:30:14 +0200

    Added constants class for the animations

---
+++ Scripts/Helper/Constants/Animations.cs
@@ -0,0 +1,13 @@
+namespace PoorManRTS.Helper.Constants;
+public static class Animations
+{
+    public static class Peasant
+    {
+        public const string IDLE = "idle";
+        public const string MINE = "mine";
+        public const string MOVE = "move";
+        public const string CUT_TREE = "cut_trees";
+        public const string CARRY_RESOURCES_IDLE = "carry_resources_idle";
+        public const string CARRY_RESOURCES_MOVE = "carry_resources_move";
+    }    
+}
